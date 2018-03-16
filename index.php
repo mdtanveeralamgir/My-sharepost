71f4e66f5144ce767b8d0ec36f621245f0ed8979
mdtanveeralamgir <mdtanveeralamgir@gmail.com>
2018-03-15 21:32:49 -0400

    app/views/pages

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
-<?php
-
-require_once "../app/bootstrap.php";
-
-//initiate core library
-
-$init = new Core;
-
-?>
+<?php require  APPROOT . '/views/inc/header.php'; ?>
+<div class="jumbotron jumbotron-flud text-center">
+	<div class="container">
+		<h1 class="display-3"><?php echo $data['title'] . "<br />"; ?></h1>
+		<p class="lead"><?php echo $data['description']; ?></p>
+		
+	</div>
+</div>
+<?php require  APPROOT . '/views/inc/footer.php'; ?>
