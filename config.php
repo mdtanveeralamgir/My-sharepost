1658b902e4499d1d9354570c105def995bec45fc
mdtanveeralamgir <mdtanveeralamgir@gmail.com>
2018-03-15 21:29:19 -0400

    app/config.php

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+//DB params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+define('DB_NAME', 'shareposts');
+
+// app root
+define('APPROOT', dirname(dirname(__FILE__)));
+
+// URL root
+define('URLROOT', 'http://localhost/~Opel/shareposts');
+
+//site name
+define('SITENAME', 'SharePosts');
+
+//version
+define('VERSION', '1.0.0');
+
+
+
+?>
