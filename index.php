c14aba86d28127a1be8b14efdb23777a4857d723
mdtanveeralamgir <mdtanveeralamgir@gmail.com>
2018-03-15 21:33:23 -0400

    app/views/posts

--- index.php
+++ index.php
@@ -1,9 +1,24 @@
 <?php require  APPROOT . '/views/inc/header.php'; ?>
-<div class="jumbotron jumbotron-flud text-center">
-	<div class="container">
-		<h1 class="display-3"><?php echo $data['title'] . "<br />"; ?></h1>
-		<p class="lead"><?php echo $data['description']; ?></p>
-		
+<?php flash('post_message');?>
+<div class="row">
+	<div class="col-md-6 mb-3">
+		<h1>Posts</h1>
+	</div>
+	<div class="col-md-6">
+		<a href="<?php echo URLROOT; ?>/posts/add" class="btn btn-primary pull-right">
+			<i class="fa fa-pencil"></i> Add Posts
+		</a>
 	</div>
 </div>
+<?php foreach($data['posts'] as $post) : ?>
+<div class="card card-body mb-3">
+	<h4 class="card-title"><?php echo $post->title; ?></h4>
+	<div class="bg-light p-2 mb-3">
+		Written by <?php echo $post->name; ?> on <?php echo $post->postCreated; ?>
+	</div>
+	<p class="card-text"><?php echo $post->body; ?></p>
+	<a href="<?php echo URLROOT?>/posts/show/<?php echo $post->postId; ?>" class="btn btn-dark">More</a>
+</div>
+<?php endforeach; ?>	
+
 <?php require  APPROOT . '/views/inc/footer.php'; ?>
